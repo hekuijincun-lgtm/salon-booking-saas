55bf44de698c2b085f18962520611f7fe0725b58
hekuijincun-lgtm <hekuijincun@gmail.com>
2025-09-15 11:27:53 +0900

    Update health.ts

--- functions/health.ts
+++ functions/health.ts
@@ -1,8 +1,3 @@
+// /functions/health.ts
 export const onRequestGet: PagesFunction = async () =>
-  new Response("ok", {
-    headers: {
-      "content-type": "text/plain; charset=utf-8",
-      "cache-control": "no-store",
-    },
-  });
-
+  new Response("ok", { headers: { "content-type": "text/plain" } });
