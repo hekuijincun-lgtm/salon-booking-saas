b0b1b0e34f1c38af414fa66e0cc6f4eb2f5922c3
Kazuki <your-email@example.com>
2025-09-17 17:14:52 +0900

    chore: disable Pages Functions to force Advanced Mode _worker.js

--- functions/_middleware.ts
+++
@@ -1,9 +0,0 @@
-﻿// functions/_middleware.ts
-export const onRequest: PagesFunction = async ({ request, next }) => {
-  const { pathname } = new URL(request.url);
-  // ここは必ず Functions に通す
-  if (pathname.startsWith("/line/") || pathname.startsWith("/debug/")) {
-    return next();
-  }
-  return next();
-};
