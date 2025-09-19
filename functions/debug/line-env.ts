e77b0ba270194a5831689ccb4556d943032e4d75
hekuijincun-lgtm <hekuijincun@gmail.com>
2025-09-19 12:15:34 +0900

    Update line-env.ts

--- functions/debug/line-env.ts
+++ functions/debug/line-env.ts
@@ -1,7 +1,9 @@
-﻿// functions/debug/line-env.ts
-export const onRequest: PagesFunction<{ LINE_CHANNEL_SECRET:string; LINE_CHANNEL_ACCESS_TOKEN:string; }> = async ({ env }) => {
-  const s = env.LINE_CHANNEL_SECRET || "";
-  const t = env.LINE_CHANNEL_ACCESS_TOKEN || "";
-  const json = { secretLen: s.length, secretHead: s.slice(0,4), secretTail: s.slice(-4), tokenLen: t.length };
-  return new Response(JSON.stringify(json), { headers: { "content-type":"application/json" }});
+export const onRequestGet: PagesFunction = async ({ env }) => {
+  return new Response(
+    JSON.stringify({
+      ok: true,
+      LINE_CHANNEL_SECRET_set: !!env.LINE_CHANNEL_SECRET,
+    }),
+    { headers: { "content-type": "application/json" } }
+  );
 };
